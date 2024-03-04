ba07539d68ac93f85ea77fd3d405963025e7ecc5
Vishal Vadhiya <121622420+vishalvadhiya18@users.noreply.github.com>
2024-03-04 19:36:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "internship1802"  
+    bucket         = "interndemobucket"  
     key            = "terraform.tfstate" 
     region         = "ap-south-1" 
   }
